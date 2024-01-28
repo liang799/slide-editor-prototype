1358d3c500c1acb3b0e5a289e1681d1938c21abe
liang799 <71926348+liang799@users.noreply.github.com>
2024-01-28 16:28:58 +0800

    Renamed to slide image

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/navbar";
 import SidePanel from "@/components/side-panel";
-import Slide from "@/components/slide";
+import SlideImage from "@/components/slide-image";
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
       <SidePanel />
       <div className="p-4 mt-14 sm:ml-64">
         <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-          <Slide />
+          <SlideImage />
         </div>
       </div>
     </>
