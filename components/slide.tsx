738b09bbeb32bc322a92c4b6e9a1b10457508808
liang799 <71926348+liang799@users.noreply.github.com>
2024-01-28 14:51:04 +0800

    Simple implementation with dragging and resizing

---
+++ components/slide.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import React, { useState } from 'react';
+import Draggable from 'react-draggable';
+import { Resizable } from 're-resizable';
+
+const imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe7Ki-ys2G_MMb_xCrY7nAf87F5ZiIOyCh4f5H_JCTTtMSMLCL'
+
+export default function Slide() {
+
+  return (
+    <Draggable>
+      <Resizable
+        defaultSize={{
+          width: 200,
+          height: 360
+        }}
+        style={{
+          background: `url(${imageUrl})`,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat'
+        }}
+        lockAspectRatio={true}
+      >
+      </Resizable>
+    </Draggable>
+
+  );
+};
