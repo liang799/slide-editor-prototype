40e0ed16d5506cd17c88291448a4f8cd3d8e5ade
liang799 <71926348+liang799@users.noreply.github.com>
2024-01-29 09:40:03 +0800

    Made slide area fixed height

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <>
       <Navbar />
       <SidePanel />
-      <div className="p-4 mt-14 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+      <div className="h-90v p-4 mt-14 sm:ml-64">
+        <div className="h-5/6 p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
           <SlideImage />
         </div>
       </div>
